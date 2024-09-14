fb387710301f7c1848b5d9605aeb6f5716b58c27
Haoxuan Fan <90523867+spike020913@users.noreply.github.com>
2024-09-14 17:04:28 -0500

    deploy-3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/Home'
-import Login from './pages/login/login'
+import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
 import { Toaster } from 'react-hot-toast'
 import { useAuthContext } from './context/authContext'
