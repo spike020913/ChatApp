b51179924d3af8314bb3274809f3bd4101bd1635
Haoxuan Fan <90523867+spike020913@users.noreply.github.com>
2024-09-14 17:02:13 -0500

    deploy-2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthContextProvider } from './context/AuthContext.jsx'
+import { AuthContextProvider } from './context/authContext.jsx'
 import { SocketContextProvider } from './context/socketContext.jsx'
 import App from './App.jsx'
 import './index.css'
